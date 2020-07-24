7591af2634dde6a259bf61947535f5982d5d7ce8
nobobobo <ianhsu1221@gmail.com>
2020-07-24 14:50:24 -0700

    put & post routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000; 
 const db = require("./models");
+const { Workout } = require('./models');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +15,24 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 
 app.get('/api/workouts', (req, res)=>{
     db.Workout.find({}).then(data=>res.json(data)).catch(err=> res.json(err));
+})
+
+app.post('/api/workouts', ({ body }, res)=>{
+    const workout = new Workout(body);
+
+    db.Workout.create(workout)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.put('/api/workouts/:id', (req, res)=>{
+    const id = mongojs.ObjectId(req.params.id);
+    const exercise = new Workout(req.body);
+    db.Workout.update({_id: id}, {$push: {exercises: exercise}}).then(data=> res.json(data)).catch(err=> res.json(err));
 })
 
 app.listen(PORT, () => {

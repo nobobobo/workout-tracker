5d3170fb69f052400bab475c122bf83ad2b1b58e
nobobobo <ianhsu1221@gmail.com>
2020-07-27 00:13:32 -0700

    README and comment

---
+++ README.md
@@ -0,0 +1,65 @@
+# Welcome to workout-tracker
+
+[![Generic badge](https://img.shields.io/badge/Version-1.0.0-GREEN.svg)](https://shields.io/)
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+1. [ Description ](#desc)
+2. [ Installation ](#install)
+3. [ Usage ](#usage)
+4. [ License ](#license)
+5. [ Contributing ](#contribute)
+6. [ Tests ](#test)
+7. [ Questions ](#question)
+
+<a name="desc"></a>
+# 1. Description 
+
+This full stack application utilizes MongoDB and Express Server to provide a workout tracker. 
+
+This tracker can track two types of excersices: Resistance & Cardio. And the user can fill the form to records what she/he did
+
+The dashboard page provides visual report of the user's activities. 
+
+![capture](capture.gif)
+
+<a name="install"></a>
+# 2. Installation 
+
+```
+npm install
+```
+
+<a name="usage"></a>
+# 3. Usage 
+
+The app is hosted at Heroku: [Workout Tracker](https://nobobobo-workout-tracker.herokuapp.com/)
+
+
+<a name="license"></a>
+# 4. License 
+
+© 2020 Noboru Hayashi All Rights Reserved.
+
+This project is MIT License licensed. 
+
+<a name="contribute"></a>
+# 5. Contributing 
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check [issues page](https://github.com/nobobobo/workout-tracker/issues).<a name="test"></a>
+# 6. Tests 
+
+```
+npm run test
+```
+
+<a name="question"></a>
+# 7. Questions
+
+![Image of nobobobo](https://github.com/nobobobo.png?size=50)
+
+If you have any question about this project, please feel free to contact **Noboru Hayashi** via [Email](mailto:ianhsu1221@gmail.com).
+
+---
+
+_This README was generated with [readme-generator](https://github.com/nobobobo/readme-generator)_
--- server.js
+++ server.js
@@ -1,40 +1,50 @@
+// import express, mongoose, mongojs, path packages
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require("path");
 const mongojs = require("mongojs");
 
+// PORT and db setup 
 const PORT = process.env.PORT || 3000; 
 const db = require("./models");
 
+// init Express server and install middlewares 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// set static path directory
 app.use(express.static("public"));
 
+// connect to MongoDb
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+// Route for exercise page
 app.get("/exercise", function (req, res) {
     res.sendFile(path.join(__dirname, "public/exercise.html"));
 });
 
+// Route for stats page
 app.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "public/stats.html"));
 });
 
+// Route for 'api/workouts', find all documents from Workout collection
 app.get('/api/workouts', (req, res)=>{
     db.Workout.find({})
         .then(data=>res.json(data))
         .catch(err=> res.json(err));
 })
 
+// Route for '/api/workouts/range'
 app.get('/api/workouts/range', (req,res)=>{
     db.Workout.find({})
         .then(data=> res.json(data))
         .catch(err=> res.json(err));
 })
 
+// Route for posting a new Workout document
 app.post('/api/workouts', ({ body }, res)=>{  
     db.Workout.create(body)
     .then(data => {
@@ -45,6 +55,7 @@ app.post('/api/workouts', ({ body }, res)=>{
     });
 });
 
+// Route for adding a new exercise to exisiting workout
 app.put('/api/workouts/:id', (req, res)=>{
     const id = mongojs.ObjectId(req.params.id);
     const exercise = req.body;
@@ -56,6 +67,8 @@ app.put('/api/workouts/:id', (req, res)=>{
         .catch(err=> res.json(err));
 })
 
+
+// start the server 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });  
